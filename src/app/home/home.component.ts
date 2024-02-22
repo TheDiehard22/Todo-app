606bf1fa22275adb71d86ccd8b63339d7da9780e
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-02-22 11:00:20 +0100

    form styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ThemePalette} from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
