4ab92995f83a843497a7e185e34d49dce3c5d9dc
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-03-07 10:28:46 +0100

    item delete button werkt

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,6 @@ import { TodoItem } from './todo-item';
 export class DataService {
 
     private counter = 1;  // Private variabele voor het genereren van unieke ID's
-
     items2 = signal([] as TodoItem[]);  // Observable array van TodoItem objecten
 
     addItem(item: TodoItem) {   // Voegt een nieuw TodoItem toe aan de lijst
@@ -16,6 +15,10 @@ export class DataService {
             const newItems = items.concat({ id, ...item });  // Creëer een nieuwe array met het nieuwe item
             return newItems; // Retourneer de nieuwe items array om de signal te updaten
         });
+    }
+
+    deleteItem(id: number) {  // Verwijdert een TodoItem uit de lijst op basis van ID
+        this.items2.update(items => items.filter(item => item.id !== id)); // Filter het item met het opgegeven ID
     }
     
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { DataService } from '../data.service';
 import { MatIconModule } from '@angular/material/icon';
+import { TodoItem } from '../todo-item';
 
 
 @Component({
@@ -14,21 +15,21 @@ import { MatIconModule } from '@angular/material/icon';
       <h2 class="title-list">Todo List</h2>
       <div class="a-container">
         <ol>
-            @for (item of dataService.items2(); track item.id){
+          @for (item of dataService.items2(); track item.id){
+            <div class="list-item">
               <a [routerLink]="['/details', item.id]" matListItemTitle>
                 <li>
                   <div>
                     <p>{{ item.title }}</p>
                     <p matListItemLine>{{ item.status }}</p>
                   </div>
-                  <div>
-                    <button mat-icon-button (click)="(item)">
-                      <mat-icon>delete</mat-icon>
-                    </button>
-                  </div>
                 </li>
               </a>
-            }
+              <button mat-icon-button (click)="deleteItem(item)">
+                <mat-icon>delete</mat-icon>
+              </button>
+            </div>
+          }
         </ol>
       </div>
     </article>
@@ -37,7 +38,14 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor(public dataService: DataService) { }
-  ngOnInit() { }
+  constructor(public dataService: DataService, private router: Router) { }
 
+  ngOnInit() { }
+  deleteItem(item: TodoItem) {
+    const itemId = item?.id; // Use optional chaining to get the id
+    if (itemId !== undefined) { // Check if the id is not undefined
+      this.dataService.deleteItem(itemId);
+      this.router.navigate(['/']);
+    }
+  }
 }
