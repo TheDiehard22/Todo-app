d043555581ecb3928011da3640aecac33717dff2
Justin Breg <justin.breg@abovo.nl>
2024-03-18 10:11:32 +0100

    comments

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -27,27 +27,38 @@ export class TodoService {
   updateItem(id: number, newItem: UpdateTodoItem) {
     console.log(id, newItem);
 
+    // it would probably be better to use a .set() method on the signal instead of .update()
+    // update is meant to be used when you want to return a new value based on the old value
+
+    // excersise: refactor this to use .set() instead of .update()
     this.items.update(items => {
       const index = items.findIndex(item => item.id === id);
-      const { id: _id, status, title } = items[index];
-      const { comment, status: newStatus } = newItem;
 
-      if (index === -1) {
+      if (index === -1) { // could not find the item to update
         return items;
       }
 
-      const _newItem = {
-        ...items[index],
-        status: newStatus || items[index].status,
-        title: title || items[index].title,
-        comments: comment ? items[index].comments.concat(comment) : items[index].comments
+      const existingItem = items[index]; // grab copy of existing item
+      const { comment, status: newStatus } = newItem; // grab new status and comment
+
+      // create a new item with the updated status and comments
+      const updatedItem = {
+        ...existingItem,
+        status: newStatus || existingItem.status, // use the new status or fall back to the existing status
+        comments: comment ? [...existingItem.comments, comment] : existingItem.comments // add the new comment to the existing comments or keep the existing comments
       } as TodoItem;
 
-      const newItems = [...items];
+      const updatedItems = [...items]; // create a shallow copy of the items array
+      updatedItems[index] = updatedItem; // replace the existing item with the updated item
 
-      newItems[index] = _newItem;
+      return updatedItems;
 
-      return newItems;
+      // this does not work (previous implementation)
+
+      // items[index] = updatedItem;
+      // return items;
+
+      // It does not work because we are mutating the original array and not returning a new array
     });
   }
 
